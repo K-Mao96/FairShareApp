e908a55c0ffcfed35b0b3269a12d212ff3d194bd
K.M <indiangirl.0914@gmail.com>
2023-07-23 10:31:55 +0900

    不要なコードを削除

--- App.jsx
+++ App.jsx
@@ -1,21 +1,9 @@
 import React from 'react';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
-
-// 割り勘アプリ
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons/Ionicons';
-//
 
-import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
-import LogInScreen from './src/screens/LoginScreen';
-import SignUpScreen from './src/screens/SignUpScreen';
-
-// 割り勘アプリ
 import HomeScreen from './src/screens/HomeScreen';
 import ReimbursementListScreen from './src/screens/ReimbursementListScreen';
 import ReimbursementCreateScreen from './src/screens/ReimbursementCreateScreen';
@@ -24,13 +12,9 @@ import OthersSettingScreen from './src/screens/OthersSettingScreen';
 import DeleteDialog from './src/components/DeleteDialog';
 import AccountCreateScreen from './src/screens/AccountCreateScreen';
 import WalletCreateScreen from './src/screens/WalletCreateScreen';
-//
 
 const Stack = createStackNavigator();
-
-// 割り勘アプリ
 const Tab = createBottomTabNavigator();
-//
 
 export default function App() {
   return (
@@ -48,24 +32,6 @@ export default function App() {
     //       gestureDirection: 'horizontal',
     //     }}
     //   >
-    //     <Stack.Screen name="MemoList" component={MemoListScreen} />
-    //     <Stack.Screen name="MemoDetail" component={MemoDetailScreen} />
-    //     <Stack.Screen name="MemoEdit" component={MemoEditScreen} />
-    //     <Stack.Screen name="MemoCreate" component={MemoCreateScreen} />
-    //     <Stack.Screen
-    //       name="LogIn"
-    //       component={LogInScreen}
-    //       options={{
-    //         cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
-    //       }}
-    //     />
-    //     <Stack.Screen
-    //       name="SignUp"
-    //       component={SignUpScreen}
-    //       options={{
-    //         cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
-    //       }}
-    //     />
 
     //     {/* 割り勘アプリ */}
     //     <Stack.Screen name="Home" component={HomeScreen} />
